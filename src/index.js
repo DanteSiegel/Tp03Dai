d06903d0ee3c3499aed06f3bcffda8b38945870b
DANTE SIEGEL <47492509@ort>
2024-04-12 09:02:51 -0300

    Formatodadadai

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import Alumno from "./models/alumno.js"
 import {sumar, restar, multiplicar, dividir} from "./modules/matematica.js"
 import {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID} from
 "./modules/omdb-wrapper.js"
+
+import express  from "express"
+import res from "express/lib/response.js";
+const app = express();
+const port = 3000
+
+app.get("/", (req, res) => {res.send("Hola")} ) 
+
+app.get("/", (req, res) => {res.send("Hola")} ) 
+
+app.get("/", (req, res) => {res.send("Hola")} ) 
+
+app.listen(port, () => {   console.log(`Listening on http://localhost:${port}`)})
